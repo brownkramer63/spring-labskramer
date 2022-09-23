76c9965115e86ad708dcb034a7d063eba119a817
Kramer Brown <brownkramer63@gmail.com>
2022-09-23 11:37:27 -0500

    lab04

--- lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/Controller/CartController.java
+++ lab-04-springmvc/src/main/java/com/cydeo/lab04springmvc/Controller/CartController.java
@@ -1,9 +1,27 @@
 package com.cydeo.lab04springmvc.Controller;
 
+import com.cydeo.lab04springmvc.service.CartService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class CartController {
 
+    //need to inject cart object
+    private final CartService cartService;
+
+    //we use cartservice instead of cartserviceimpl for loosely coupled
+    public CartController(CartService cartService) {
+        this.cartService = cartService;
+    }
+
+    @RequestMapping("/cart-list")
+    public String listCarts(Model model){
+
+        model.addAttribute("cartList",cartService.retrieveCartList());
+
+        return "cart/cart-list";
+    }
 
 }

128b9c4985ecb32fc3f5b3e861c933948a530de4
Kramer Brown <brownkramer63@gmail.com>
2022-11-12 14:59:43 -0600

    entitys

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/AddressRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/AddressRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
-public interface AddressRepository{
+public interface AddressRepository {
     //Write a derived query to get all address with a specific customer
     //Write a derived query to get address with a specific street
     //Write a derived query to get top 3 address with a specific customer email

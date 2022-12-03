8e9e14e71202eab48049c9ad74698a5355f2b01b
Kramer Brown <brownkramer63@gmail.com>
2022-12-02 19:11:39 -0600

    labAPI

--- lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/product_Controller.java
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/controller/product_Controller.java
@@ -1,11 +1,28 @@
 package com.cydeo.lab08rest.controller;
 
-import com.cydeo.lab08rest.repository.ProductRepository;
+import com.cydeo.lab08rest.dto.ProductDTO;
+import com.cydeo.lab08rest.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/product")
 public class product_Controller {
 
+private final ProductService productService;
+
+
+    public product_Controller(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public List<ProductDTO> getProductList(){
+        return productService.getProducts();
+    }
 
 
 
